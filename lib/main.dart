2fe9684a7f5bcc490a035b5702c9df3995c84bd3
Disha Shah <disha.shah1198@gmail.com>
2023-03-22 23:04:14 -0400

    cleaned up route

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:my_flutterapp/constants/routes.dart';
 import 'package:my_flutterapp/views/register_view.dart';
 import 'package:my_flutterapp/views/verify_view.dart';
 import 'firebase_options.dart';
@@ -16,9 +17,9 @@ void main() {
       ),
       home: const HomePage(),
       routes: {
-        '/login/': (context) => const LoginView(),
-        '/register/': (context) => const RegisterView(),
-        '/notes/': (context) => const NotesView(),
+        loginRoute: (context) => const LoginView(),
+        registerRoute: (context) => const RegisterView(),
+        notesRoute: (context) => const NotesView(),
 
       },
     ),
@@ -79,7 +80,7 @@ class _NotesViewState extends State<NotesView> {
                 if(shouldLogout){
                 await FirebaseAuth.instance.signOut();
                 Navigator.of(context).pushNamedAndRemoveUntil(
-                  '/login/', 
+                  loginRoute, 
                   (_) => false);
                 }
                   break;
